8f6e79e51699eae099500a6fb16c4c9fd3e6c9c2
Vic <v235711@gmail.com>
2017-12-09 01:55:28 -0200

    add howToUsePointers.cpp

---
+++ howToUsePointers.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x = 100;
+    int *p = &x;
+    int *q = p;
+    
+    cout<<x<<' '<<&x<<endl;
+    cout<<p<<' '<<*p<<' '<<&p<<endl;
+    cout<<q<<' '<<*q<<endl;
+    // *q prints the value at the memory address of variable q. but q holds the memory address of p.
+    // but p holds the memory address of x. so shouldn't *q print an address?
+    
+    //p is the address of x. p is a pointer
+    // *p is the value that the address of memory p is. In this case, it's x address.
+    // &p is p address
+    
+    std::cout << x << '\n'; // print the value of variable x
+    std::cout << &x << '\n'; // print the memory address of variable x
+    std::cout << *&x << '\n'; /// print the value at the memory address of variable x
+    
+    int value = 5;
+    int *ptr = &value; // ptr points to value
+    *ptr = 7; // *ptr is the same as value, which is assigned 7
+    std::cout << value; // prints 7
+}
