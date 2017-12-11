372201ce6463ef8b472fb9e821581d9db333e2ed
Vic <v235711@gmail.com>
2017-12-11 00:58:07 -0200

    .

--- Using_Mallocs_With_Pointers.cpp
+++ Using_Mallocs_With_Pointers.cpp
@@ -8,14 +8,18 @@ struct pointer_with_attributes{
 };
 
 int main() {
+	
 	pointer_with_attributes* my_pointer = NULL;
 	
+	// precisa colocar algum address pro pointer
 	my_pointer = (struct pointer_with_attributes*)malloc(sizeof(struct pointer_with_attributes));
-	
+
 	my_pointer->id_of_pointer = 1983;
-	my_pointer->name_of_pointer = "Spongebob";
+	my_pointer->name_of_pointer = "Vic";
 	
+	// 2 ways to access the pointer stuff (attributes)
 	printf(" %d ", my_pointer->id_of_pointer);
 	cout<<(*my_pointer).name_of_pointer;
+
 	return 0;
 }
